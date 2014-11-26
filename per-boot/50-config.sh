7b71b923e41231edbb9b114e8f4b3ca877e87a3c
Dan Bedard <beppodb@users.noreply.github.com>
2014-11-26 17:14:38 -0500

    50-config.sh was missing a single quote

--- per-boot/50-config.sh
+++ per-boot/50-config.sh
@@ -2,7 +2,7 @@
 FQDN=`ec2metadata --public-hostname`
 ZONENAME="tempZone"
 
-sudo sed -i 's/serverURL.*/serverURL = \"http:\/\/'"$FQDN"\" \}/g;
+sudo sed -i 's/serverURL.*/serverURL = \"http:\/\/'"$FQDN"'\" \}/g;
     s/idrop.config.preset.zone=.*/idrop.config.preset.zone=\"'"$ZONENAME"'\"/g' /etc/idrop-web/idrop-web-config2.groovy
 sudo service tomcat6 restart
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
