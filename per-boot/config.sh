34bfa6fd9ad88ea7dc03a23ee960ea578893ae4a
Dan Bedard <danb@renci.org>
2014-11-26 05:57:21 +0000

    Reorg and updates.

---
+++ per-boot/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FQDN=`ec2metadata --public-hostname`
+ZONENAME="tempZone"
+
+sudo sed -i 's/serverURL.*/serverURL = \"http:\/\/'"$FQDN"':'"80"'\/idrop-web2\" \}/g;
+    s/idrop.config.preset.zone=.*/idrop.config.preset.zone=\"'"$ZONENAME"'\"/g' /etc/idrop-web/idrop-web-config2.groovy
+sudo service tomcat6 restart
