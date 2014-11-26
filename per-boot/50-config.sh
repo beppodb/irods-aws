3d58f497d04abd1e123d86ceb9dc7459ce89da9d
Dan Bedard <danb@renci.org>
2014-11-26 06:43:25 +0000

    Updated tomcat port#

--- per-boot/50-config.sh
+++ per-boot/50-config.sh
@@ -5,3 +5,4 @@ ZONENAME="tempZone"
 sudo sed -i 's/serverURL.*/serverURL = \"http:\/\/'"$FQDN"':'"80"'\/idrop-web2\" \}/g;
     s/idrop.config.preset.zone=.*/idrop.config.preset.zone=\"'"$ZONENAME"'\"/g' /etc/idrop-web/idrop-web-config2.groovy
 sudo service tomcat6 restart
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
--- per-once/build.sh
+++ per-once/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get -y install postgresql openjdk-6-jdk tomcat6
 wget ftp://ftp.renci.org/pub/irods/releases/4.0.3/irods-icat-4.0.3-64bit.deb
 wget ftp://ftp.renci.org/pub/irods/releases/4.0.3/irods-database-plugin-postgres-1.3.deb
-sudo apt-get -y install postgresql openjdk-6-jdk tomcat6
 sudo dpkg -i irods-database-plugin-postgres-1.3.deb irods-icat-4.0.3-64bit.deb
-sudo apt-get -f install
+sudo apt-get -f -y install
 wget http://people.renci.org/~danb/FOR_DEMOS/iDrop-Web-2/idrop-web2.war
-mkdir /etc/idrop-web
-mkdir /etc/tomcat6
-cp /home/ubuntu/server.xml /etc/tomcat6
-cp /home/ubuntu/idrop-web-config2.groovy /etc/idrop-web
-
+sudo mkdir /etc/idrop-web
+sudo mkdir /etc/tomcat6
+sudo cp ./server.xml /etc/tomcat6
+sudo cp ./idrop-web-config2.groovy /etc/idrop-web
+sudo cp ./idrop-web2.war /var/lib/tomcat6/webapps
