701cdc01a5c5bae2d525def6eedca5d1cd5cc3af
Dan Bedard <danb@renci.org>
2015-03-27 21:24:38 -0400

    Admin password and db password set to ec2 instance id

--- per-instance/50-genresp.sh
+++ per-instance/50-genresp.sh
@@ -5,9 +5,10 @@
 # Zone SID, agent key, database admin, and admin password are all randomized.
 
 RESPFILE="/opt/irods-aws/setup_responses"
+EC2_INSTANCE_ID=$(ec2metadata --instance-id)
 
 echo "irods" > $RESPFILE                 # service account user ID
-echo "irods" >> $RESPFILE                # service account password
+echo "irods" >> $RESPFILE                # service account group ID
 echo "tempZone" >> $RESPFILE             # initial zone name
 echo "1247" >> $RESPFILE                 # service port #
 echo "20000" >> $RESPFILE                # transport starting port #
@@ -17,11 +18,11 @@ echo "/var/lib/irods/Vault" >> $RESPFILE # vault path
  | tr -d '\n' ; echo "-SID") >> $RESPFILE # zone SID
 openssl rand -base64 32 | sed 's,/,S,g' | cut -c 1-32 >> $RESPFILE # agent key
 echo "rods" >> $RESPFILE                  # iRODS admin account
-openssl rand -base64 16 | sed 's,/,S,g' | cut -c 1-16 >> $RESPFILE      # iRODS admin password
+echo $EC2_INSTANCE_ID >> $ RESPFILE       # iRODS admin password
 echo "yes" >> $RESPFILE                   # confirm iRODS settings
 echo "localhost" >> $RESPFILE             # database hostname
 echo "5432" >> $RESPFILE                  # database port
 echo "ICAT" >> $RESPFILE                  # database DB name
 echo "irods" >> $RESPFILE                 # database admin username
-openssl rand -base64 16 | sed 's,/,S,g' | cut -c 1-16 >> $RESPFILE        # database admin password
+echo $EC2_INSTANCE_ID >> $ RESPFILE       # database admin password
 echo "yes" >> $RESPFILE                   # confirm database settings
